33251d88d821b3a8701a28c6ba5ff0687405cdfa
Simon Fugere <smonyk@gmail.com>
2016-06-20 02:34:42 -0400

    updated the about section. created data files for content

--- resources/js/controllers/aboutCtrl.js
+++ resources/js/controllers/aboutCtrl.js
@@ -4,6 +4,6 @@ angular.module('wendyApp')
 
 	$scope.about = {
 		image : 'resources/images/portrait.jpg',
-		copy : 'My name is Wendy.\n I\’m a former graduate of linguistics and rhetorical studies, and have a working knowledge of at least four languages.\n I\'m an aspiring art director with skills in design. I believe fervently that the world can change for the better, and would love to do that through advertising.\n In my spare time, you can find me walking my dog, rock climbing, or just cycling around the city and enjoying a good cup of joe with a friend.'
+		copy : 'My name is Wendy.\n\nI\’m a former graduate of linguistics and rhetorical studies, and have a working knowledge of at least four languages.\n\nI\'m an aspiring art director with skills in design. I believe fervently that the world can change for the better, and would love to do that through advertising.\n\nIn my spare time, you can find me walking my dog, rock climbing, or just cycling around the city and enjoying a good cup of joe with a friend.'
 	};
 });
